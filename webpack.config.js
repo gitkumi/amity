b15c48c621c013a0b38f0806c979ae52ce89af5c
Takumi <ito.tkmi@gmail.com>
2018-12-25 04:08:02 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader', 
+            'sass-loader'
+          ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  // optimization: {
+  //   minimize: false
+  // },
+  devServer: {
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist', 'build/*.*']),
+    new MiniCssExtractPlugin({
+      filename: 'index.css'
+    }),
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
