c0ef5ecc8ea52b5f3531dbb226c501885fe52e92
Takumi <ito.tkmi@gmail.com>
2018-12-27 02:11:05 +0800

    Add sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import '../styles/_all.scss';
+
+// const addEventListener = () => {
+//   document.getElementById('cookies__button').addEventListener('click', () => {
+//     document.getElementById('cookies').style.display = 'none';
+//   });
+// }
+
+// addEventListener();
