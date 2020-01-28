f62e3ab4178a44de54810baa076c6a76472f74fa
Takumi <ito.tkmi@gmail.com>
2020-01-29 01:19:10 +0900

    Update node packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -32,10 +32,11 @@ module.exports = {
     ]
   },
   devServer: {
-    hot: true
+    compress: true,
+    port: 9000
   },
   plugins: [
-    new CleanWebpackPlugin(['dist', 'build/*.*']),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'index.css'
     }),
